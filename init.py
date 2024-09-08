2625eec8b8032ffa9b7b2b8589e10eaf70180704
jaejin <jaejin960729@gmail.com>
2024-09-09 03:07:14 +0900

    .
    .

--- init.py
+++ init.py
@@ -36,24 +36,52 @@ while cv2.waitKey(33) < 0:
 capture.release()
 cv2.destroyAllWindows()
 '''
-'''
+
+
 if __name__ == '__main__' :
+    import cv2
+    import os
     from ultralytics import YOLO
+
 
     # Load a model
     model = YOLO("yolov8m.yaml")  # build a new model from scratch
     model = YOLO("yolov8m.pt")  # load a pretrained model (recommended for training)
 
     # Use the model
-    model.train(data="coco128.yaml", epochs=3)  # train the model
+    model.train(data="./datasets/car/test.yaml", epochs=10)  # train the model
     metrics = model.val()  # evaluate model performance on the validation set
+
+    ##################
+
+    video_paths = os.getcwd() + "/datasets/car/v3.mp4"
+    cap = cv2.VideoCapture(video_paths)
+
+    while cap.isOpened():
+        success, frame = cap.read()
+
+        if success:
+            results = model(frame)
+
+            annotated_frame = results[0].plot()
+
+            cv2.imshow("YOLOv8 inference", annotated_frame)
+
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        else:
+            break
+    cap.release()
+    cv2.destroyAllWindows()
     # results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
-    results = model(os.getcwd()+"/datasets/car/testImage/testImage_000000.jpg")  # predict on an image
+    # results = model(os.getcwd()+"/datasets/car/testImage/testImage_000000.jpg")  # predict on an image
 
     path = model.export(format="onnx")  # export the model to ONNX format
 
-'''
 
+
+
+'''
 import cv2
 import os
 from ultralytics import  YOLO
@@ -79,6 +107,8 @@ while cap.isOpened():
         break
 cap.release()
 cv2.destroyAllWindows()
+'''
+
 
 
 
